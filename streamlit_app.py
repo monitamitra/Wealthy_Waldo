72955df16aad1544008846ba45f5ef5d1d9c43f1
Monita <mitramonita@gmail.com>
2024-07-11 09:44:18 -0400

    still trying to fix chromadb issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+from langchain_community.vectorstores import Chroma
 __import__('pysqlite3') 
 import sys 
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
@@ -12,7 +13,6 @@ from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain.agents import AgentExecutor, create_react_agent
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_community.document_loaders import TextLoader
-from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAI
 
 
