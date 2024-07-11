240457338d8bd5bf87361e04fc4676ccbfb8af28
Monita <mitramonita@gmail.com>
2024-07-11 08:51:25 -0400

    testing initialization of chrmadb

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,76 +32,83 @@ embeddings = HuggingFaceBgeEmbeddings(
     model_kwargs={"device": "cpu"},
     encode_kwargs={"normalize_embeddings": True},
 )
-db = Chroma.from_documents(doc_splits, embeddings)
+# db = Chroma.from_documents(doc_splits, embeddings)
 
-# define tools for langchain agent to use => tavily to search internet and chromadb to store vector embeddings
-vector_store_retriever = db.as_retriever()
-retriever_tool = create_retriever_tool(
-    vector_store_retriever,
-    "Asset class knowledge base",
-    """Search for specific information about different asset classes in investment portfolios. 
-    For generating a specific investment portfolio, you must use this tool!"""
-)
+# # define tools for langchain agent to use => tavily to search internet and chromadb to store vector embeddings
+# vector_store_retriever = db.as_retriever()
+# retriever_tool = create_retriever_tool(
+#     vector_store_retriever,
+#     "Asset class knowledge base",
+#     """Search for specific information about different asset classes in investment portfolios. 
+#     For generating a specific investment portfolio, you must use this tool!"""
+# )
 
-web_search_tool = TavilySearchResults(max_results = 4)
-web_search_tool.description = """find relevant information fromt the internet needed to contruct the user's 
-    investment portfolio"""
+# web_search_tool = TavilySearchResults(max_results = 4)
+# web_search_tool.description = """find relevant information fromt the internet needed to contruct the user's 
+#     investment portfolio"""
 
-tools = [web_search_tool, retriever_tool]
+# tools = [web_search_tool, retriever_tool]
 
-# prompt template telling the llm what it does 
-prompt_str_template = """your name is Wealthy Waldo. You are an investment planning assistant who generates a 
-    personalized and specific investment portfolio for a user based on the characteristics of their profile. 
-    Given a user with a {risk_tolerance} risk tolerance, {investment_goal} investment goal, 
-    and a {investment_horizon} investment horizon, and considering the current market data and respective news for 
-    specific asset classes that you feel are necessary, your job is to generate a diversified investment portfolio 
-    that aligns with the user's preferences. Prioritize assets with {investment_style} investment style 
-    characteristics.First use tools to search the internet for a general investment portfolio plan and then 
-    for each asset class use your asset class knowledge base to search for specific information about 
-    different asset classes in investment portfolios. You can use necessary tools to accompish your task.
-    The output should be like this: 
-    **Overall Asset Allocation :
-    * asset_class_1: allocation_1%
-    * asset_class_2: allocation_2%
-    * ... and so on for all asset classes
+# # prompt template telling the llm what it does 
+# prompt_str_template = """your name is Wealthy Waldo. You are an investment planning assistant who generates a 
+#     personalized and specific investment portfolio for a user based on the characteristics of their profile. 
+#     Given a user with a {risk_tolerance} risk tolerance, {investment_goal} investment goal, 
+#     and a {investment_horizon} investment horizon, and considering the current market data and respective news for 
+#     specific asset classes that you feel are necessary, your job is to generate a diversified investment portfolio 
+#     that aligns with the user's preferences. Prioritize assets with {investment_style} investment style 
+#     characteristics.First use tools to search the internet for a general investment portfolio plan and then 
+#     for each asset class use your asset class knowledge base to search for specific information about 
+#     different asset classes in investment portfolios. You can use necessary tools to accompish your task.
+#     The output should be like this: 
+#     **Overall Asset Allocation :
+#     * asset_class_1: allocation_1%
+#     * asset_class_2: allocation_2%
+#     * ... and so on for all asset classes
 
-    **Detailed Asset Class Breakdowns => using Vector Store**
-    **For each asset class retrieved:
-  * Query the vector store to find information on relevant subcategories and investment options specific to that asset class
-  and user's specific investment goals, investment horizon, and risk_tolerance. 
-  * Analyze the retrieved data using historical performance, risk profiles, etc  based on the asset class type.
-  * Based on this analysis and user input, recommend specific allocations for subcategories within the asset class. 
-  * Explain the rationale behind the allocation percentages for each subcategory."""
+#     **Detailed Asset Class Breakdowns => using Vector Store**
+#     **For each asset class retrieved:
+#   * Query the vector store to find information on relevant subcategories and investment options specific to that asset class
+#   and user's specific investment goals, investment horizon, and risk_tolerance. 
+#   * Analyze the retrieved data using historical performance, risk profiles, etc  based on the asset class type.
+#   * Based on this analysis and user input, recommend specific allocations for subcategories within the asset class. 
+#   * Explain the rationale behind the allocation percentages for each subcategory."""
 
-prompt_str = ""
+# prompt_str = ""
 
-# streamlit formatting
-st.title("🦜🔗 Wealthy Waldo: Your Investment Planning Assistant 💸")
+# # streamlit formatting
+# st.title("🦜🔗 Wealthy Waldo: Your Investment Planning Assistant 💸")
 
-# initiates agent action to generate portfolio
-def generate_response():
-    llm = OpenAI(openai_api_key=os.getenv("OPEN_AI_API_KEY"), temperature = 0)
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", prompt_str),
-        ("user", "{input}")])
+# # initiates agent action to generate portfolio
+# def generate_response():
+#     llm = OpenAI(openai_api_key=os.getenv("OPEN_AI_API_KEY"), temperature = 0)
+#     prompt = ChatPromptTemplate.from_messages([
+#         ("system", prompt_str),
+#         ("user", "{input}")])
     
-    agent = create_react_agent(llm, tools, prompt)
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-    result = agent_executor.invoke({"input": "generate an personalized investment portfolio for me." })
-    st.info(result.get("output"))
+#     agent = create_react_agent(llm, tools, prompt)
+#     agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+#     result = agent_executor.invoke({"input": "generate an personalized investment portfolio for me." })
+#     st.info(result.get("output"))
 
-# formats prompt template for langchain agent according to user investment profile
-with st.form('my_form'):
-    st.info('Hello! I am Wealthy Waldo! What can I do to make you wealthy today?')
+# # formats prompt template for langchain agent according to user investment profile
+# with st.form('my_form'):
+#     st.info('Hello! I am Wealthy Waldo! What can I do to make you wealthy today?')
 
-    risk_tolerance_option = st.select_slider("Risk Tolerance", options = [ "Conservative", "Moderate", "Aggressive"])
-    investment_goals = st.text_area("What are your short-term or long-term goals?")
-    investment_horizon_option = st.select_slider("Investment Horizon", options = ["Short Term (few months to 3 years)", 
-                                                                                  "Medium Term (5-10 years)", 
-                                                                                  "Long Term (at least 10 years)"])
-    investment_styles_option = st.selectbox("Investment Styles", options = ["Passive", "Active"])
-    submitted = st.form_submit_button('Submit')
-    if submitted:
-        prompt_str = prompt_str_template.format(risk_tolerance = risk_tolerance_option, investment_goal = investment_goals, 
-                               investment_horizon = investment_horizon_option, investment_style = investment_styles_option)
-        generate_response()
+#     risk_tolerance_option = st.select_slider("Risk Tolerance", options = [ "Conservative", "Moderate", "Aggressive"])
+#     investment_goals = st.text_area("What are your short-term or long-term goals?")
+#     investment_horizon_option = st.select_slider("Investment Horizon", options = ["Short Term (few months to 3 years)", 
+#                                                                                   "Medium Term (5-10 years)", 
+#                                                                                   "Long Term (at least 10 years)"])
+#     investment_styles_option = st.selectbox("Investment Styles", options = ["Passive", "Active"])
+#     submitted = st.form_submit_button('Submit')
+#     if submitted:
+#         prompt_str = prompt_str_template.format(risk_tolerance = risk_tolerance_option, investment_goal = investment_goals, 
+#                                investment_horizon = investment_horizon_option, investment_style = investment_styles_option)
+#         generate_response()
+
+try:
+    db = Chroma.from_documents(doc_splits, embeddings)
+    st.write("Chroma initialized successfully")
+except Exception as e:
+    st.write("Error initializing Chroma:")
+    st.write(e)
