13c39c61178a96371f02cde22e6811d03a9868e2
Monita <mitramonita@gmail.com>
2025-05-24 22:13:19 -0500

    debugging response variable from langgraph agent

--- app.py
+++ app.py
@@ -24,7 +24,8 @@ def generate_response(human_prompt, uploaded_file=None):
     if human_prompt is not None:
         main_agent = create_agent(uploaded_file)
         response = main_agent.invoke({"input": human_prompt})
-        return response["output"]
+        st.write("DEBUG:", response)
+        return response
 
 def create_agent(uploaded_file=None):
         tools = [vectordb_tool(), websearch_tool(), asset_performance_tool]
